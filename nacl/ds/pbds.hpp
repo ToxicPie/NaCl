8dc084fe98e8fe6be02119182c1853d083645c0b
Kent <kent05011607@gmail.com>
2021-10-08 05:05:02 +0800

    changed pbds descriptions

--- nacl/ds/pbds.hpp
+++ nacl/ds/pbds.hpp
@@ -3,7 +3,7 @@
 #include <ext/pb_ds/tree_policy.hpp>     // keep-line
 using namespace __gnu_pbds;
 
-// most of std::map + order_of_key, find_by_order
+// most std::map + order_of_key, find_by_order, split, join
 template <typename T, typename U = null_type>
 using ordered_map = tree<T, U, std::less<>, rb_tree_tag,
                          tree_order_statistics_node_update>;
@@ -16,7 +16,7 @@ template <typename T> struct myhash {
 template <typename T, typename U = null_type>
 using hash_table = gp_hash_table<T, U, myhash<T>>;
 
-// most of std::priority_queue + merge
+// most std::priority_queue + modify, erase, split, join
 using heap = priority_queue<int, std::less<>>;
 // useful tags: pairing_heap_tag, binary_heap_tag,
-// binomial_heap_tag
+//              (rc_)?binomial_heap_tag, thin_heap_tag
