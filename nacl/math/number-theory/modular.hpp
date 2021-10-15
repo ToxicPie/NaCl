2cb577c19e6214754b1112801b8000fcbdcb7c5c
Kent <kent05011607@gmail.com>
2021-10-15 21:44:10 +0800

    changed modular

--- nacl/math/number-theory/modular.hpp
+++ nacl/math/number-theory/modular.hpp
@@ -1,8 +1,9 @@
+#include "extgcd.hpp"
+
 template <typename T> struct M {
   static T MOD; // change to constexpr if already known
   T v;
-  M() : v(0) {}
-  M(T x) {
+  M(T x = 0) {
     v = (-MOD <= x && x < MOD) ? x : x % MOD;
     if (v < 0) v += MOD;
   }
@@ -14,6 +15,11 @@ template <typename T> struct M {
   M operator-(M b) { return M(v - b.v); }
   M operator*(M b) { return M((__int128)v * b.v % MOD); }
   M operator/(M b) { return *this * (b ^ (MOD - 2)); }
+  // change above implementation to this if MOD is not prime
+  M inv() {
+    auto [p, _, g] = extgcd(v, MOD);
+    return assert(g == 1), p;
+  }
   friend M operator^(M a, ll b) {
     M ans(1);
     for (; b; b >>= 1, a *= a)
